f6e5dd58dc2febfcb540ec702338279de2a403c8
Mihirpatel8603 <140783551+Mihirpatel8603@users.noreply.github.com>
2023-07-31 17:58:09 +0530

    Add files via upload

---
+++ Month.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWS_Billing_Project
+{
+    internal class Month
+    {
+        string custId = "";
+        string name = "";
+        List<Usages> usages = new List<Usages>();
+        List<Type> types = new List<Type>();
+
+        public Month() { }
+
+
+        public Month(string name, string custId)
+        {
+
+            this.name = name;
+            this.custId = custId;
+
+        }
+
+        public void addUsages(Usages usages)
+        {
+            this.usages.Add(usages);
+
+        }
+
+        public string getName()
+        {
+            return this.name;
+        }
+    }
+}
